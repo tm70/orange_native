966751ad6169c141fe925409690e32f9bbcd7e96
tm70 <stealthreamz@gmail.com>
2020-09-16 13:08:15 +1000

    Still runs rn, committing in case things break

--- src/screens/GameInviteFriends.tsx
+++ src/screens/GameInviteFriends.tsx
@@ -3,6 +3,7 @@ import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import {StackNavigationProp} from '@react-navigation/stack/lib/typescript/src/types';
 import {StackParamList} from '../../App';
 import fontScaler from '../util/fontScaler';
+import getRelationships from "../api/GetRelationships";
 import ArrowButton, {Direction} from '../components/ArrowButton';
 
 // To get the navigation prop typed
@@ -10,11 +11,28 @@ type GameInviteFriendsNavigationProp = StackNavigationProp<StackParamList, 'Game
 type GameInviteFriendsRouteProp = RouteProp<StackParamList, 'GameInviteFriends'>
 type Props = { navigation: GameInviteFriendsNavigationProp; route: GameInviteFriendsRouteProp };
 
+const Item = ({ firstname, id }) => (
+    <TouchableOpacity style={styles.item}> // add onpress that sends a game request to the api
+        <ImageBackground source={require('../../assets/person.png')} style={{}}>
+            <View style={styles.tile} />
+        </ImageBackground>  
+        <Text style={styles.title}>{firstname}</Text>
+    </TouchableOpacity>
+);
+
 const GameInviteFriends: React.FC<Props> = ({navigation, route}) => {
     const params = route.params;
+    
+    const renderItem = ({ item }) => (
+        <Item
+            firstname={item.firstname}
+            id={item.id}
+        />
+    );
+    
     return (
         <View style={styles.container}>
-            <Text style={styles.header}>Invite Friends</Text>
+            <Text style={styles.header}>Invite Friend</Text>
             
             <ArrowButton
                 text="Start Game"
