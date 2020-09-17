ab076e13639ff359d9054795ee099222a6afab14
tm70 <stealthreamz@gmail.com>
2020-09-17 11:39:26 +1000

    (untested) APIs getGames, sendGameRequest and respondGameRequest added

---
+++ src/api/sendGameRequest.ts
@@ -0,0 +1,29 @@
+import {BACKEND_BASE_URL} from "./endpoints";
+
+export interface Response {
+    message: string,
+    status: number,
+}
+
+const sendGameRequest = async (id: number, oppid: number, game: string, token: string): Promise<Response> => {
+    const url = `${BACKEND_BASE_URL}/${id}/games`;
+    
+    let response = await fetch(url, {
+        method: 'POST',
+        headers: {Authorization: `Bearer ${token}`},
+        body: {game_type: game, opponent_id: oppid},
+    });
+    
+    if (!response.ok) {
+        throw new Error('Failed to connect');
+    }
+    
+    return await response.json().then((data) => {
+        if (data.status !== 200) {
+            throw new Error(data)
+        }
+        return data;
+    })
+};
+
+export default sendGameRequest;
