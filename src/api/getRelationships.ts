cabf2990f9436d08fd48171c7830f3eeedec9652
Heng-J <heng.jiang@uqconnect.edu.au>
2020-09-17 11:38:46 +1000

    getRelationships

---
+++ src/api/getRelationships.ts
@@ -0,0 +1,28 @@
+import {BACKEND_BASE_URL} from "./endpoints";
+
+export interface Relationship {
+    user_first_id: number,
+    user_second_id: number,
+    relationship: string
+}
+
+const getRelationships = async (id: number, token: string): Promise<Relationship[]> => {
+    const url = `${BACKEND_BASE_URL}/${id}/relationships`;
+
+    let response = await fetch(url, {
+        headers: {Authorization: `Bearer ${token}`},
+    });
+
+    if (!response.ok) {
+        throw new Error('Failed to connect');
+    }
+
+    return await response.json().then((data) => {
+        if (data.status !== 200) {
+            throw new Error(data.message)
+        }
+        return data.relationships;
+    })
+};
+
+export default getRelationships;
